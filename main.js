420db9c2ecf783446f45979e33cb4d548d8efa41
alientheeight <czapski.tomasz@hotmail.com>
2024-04-08 10:05:12 +0200

    Header feature created

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 import "./scss/main.scss";
+
+function makeBurger() {
+  const btn = document.querySelector(".header__menu-burger");
+  const body = document.querySelector("body");
+  btn.onclick = () => {
+    body.classList.toggle("show-menu");
+  };
+}
+
+makeBurger();
